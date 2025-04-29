ae6601f2e80bce593277aec2116350ec6184014a
magoudi <youssefmagedan@gmail.com>
2025-04-29 14:12:52 +0300

    aloooo

--- Lexical_Analyzer.py
+++ Lexical_Analyzer.py
@@ -4,7 +4,7 @@ token_specification = [
     ('KEYWORD', r'\b(for|int|float)\b'),
     ('ID',       r'\b[a-zA-Z_][a-zA-Z_0-9]*\b'),
     ('NUM',      r'\b\d+\b'),
-    ('OP',       r'[=+<]'),
+    ('OP',       r'[=+<*-/]'),
     ('SYMBOL',   r'[{}();]'),
     ('SKIP',     r'[ \t]+'),   # Skip spaces and tabs
     ('NEWLINE',  r'\n'),
@@ -13,6 +13,8 @@ token_specification = [
 
 tok_regex = '|'.join(f'(?P<{name}>{pattern})' for name, pattern in token_specification)
 get_token = re.compile(tok_regex).match
+
+
 
 def tokenize(code):
     pos = 0
@@ -27,6 +29,40 @@ def tokenize(code):
             pos = match.end()
         else:
             raise SyntaxError(f'Unexpected character: {code[pos]}')
+    return tokens
+
+def analyze(file_path):
+    tokens = []
+
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    for line in lines:
+        line = line.strip()
+
+        # Add your logic here to tokenize each line
+        # Here's a dummy implementation for example:
+        words = line.replace('(', ' ( ').replace(')', ' ) ').replace('{', ' { ')\
+                    .replace('}', ' } ').replace(';', ' ; ').replace('=', ' = ')\
+                    .replace('+', ' + ').replace('-', ' - ').replace('*', ' * ')\
+                    .replace('/', ' / ').replace('<', ' < ').split()
+
+        for word in words:
+            if word in ['int', 'float', 'for']:
+                tokens.append((word, 'KEYWORD'))
+            elif word in ['(', ')', '{', '}', ';']:
+                tokens.append((word, 'SYMBOL'))
+            elif word in ['=', '+', '-', '*', '/', '<', '>', '==', '<=', '>=', '!=']:
+                tokens.append((word, 'OPERATOR'))
+            elif word.isdigit():
+                tokens.append((word, 'NUMBER'))
+            elif word.replace('.', '', 1).isdigit():
+                tokens.append((word, 'NUMBER'))
+            elif word.isidentifier():
+                tokens.append((word, 'IDENTIFIER'))
+            else:
+                tokens.append((word, 'UNKNOWN'))
+
     return tokens
 
 try:
